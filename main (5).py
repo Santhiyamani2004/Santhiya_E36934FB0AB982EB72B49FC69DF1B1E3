5af48d3634b536a707a6a257ac6a7f476337e949
Santhiyamani2004 <148053453+Santhiyamani2004@users.noreply.github.com>
2023-10-16 14:55:16 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,6 @@
+#python program to check leap year
+Year =int(input("2012")
+If(year%4==0 and year%100!=0 or year%400==0):
+  Print("the year is a leap year!")
+Else:
+  Print("the year is not leap year !")
